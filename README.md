3d015032340f4471fd694a9e5656b09da65124d8
Delan Azabani <dazabani@igalia.com>
2023-10-25 19:02:07 +0800

    blog: This month in Servo (October 2023) (#122)
    * blog: This month in Servo (October 2023)
    
    * write (almost) the rest of the post
    
    * add screenshot of GitHub label menu
    
    * add list-commits-by-nightly.sh
    
    * clean up and rename post
    
    * remove stuff about try job labels for now
    
    * shorten summary a bit
    
    * change NLNet link to grant page
    
    Co-authored-by: Manuel Rego Casasnovas <rego@igalia.com>
    
    * add missing details to conferences section
    
    * fix link in upgrades section
    
    ---------
    
    Co-authored-by: Manuel Rego Casasnovas <rego@igalia.com>

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 1. `npm install`
 2. `npm run start`
+
+## How to list commits that landed in each nightly
+
+Note that the list of nightly builds is cached in tools/runs.json, so if it exists, you will need to delete it to fetch an updated list.
+
+```sh
+$ tools/list-commits-by-nightly.sh /path/to/servo
+```
---
+++ tools/list-commits-by-nightly.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+# usage: list-commits-by-nightly.sh <path/to/servo>
+# requires: zsh, gh, jq, tac, rg, git
+set -eu
+if [ $# -lt 1 ]; then >&2 sed '1d;2s/^# //;2q' "$0"; exit 1; fi
+missing() { >&2 echo "fatal: $1 not found"; exit 1; }
+> /dev/null command -v gh || missing gh
+> /dev/null command -v jq || missing jq
+> /dev/null command -v tac || missing tac
+> /dev/null command -v rg || missing rg
+> /dev/null command -v git || missing git
+cd -- "$(dirname -- "${0:a}")"
+
+if ! [ -e runs.json ]; then
+  gh api '/repos/servo/servo/actions/workflows/nightly.yml/runs?status=success&per_page=100' > runs.json
+fi
+< runs.json jq -r '.workflow_runs[] | "\(.head_sha)\t\(.updated_at)"' | tac > runs.tsv
+< runs.tsv sed -En '1!{H;x;s/\n//;p;x;};s/\t.*//;s/$/\t/;h' \
+| while read -r from to updated; do
+  printf '>>> %s\n' "$updated" | rg --color=always .  # make it red
+  git -C "$1" log --pretty=$'tformat:%H\t%s' $from..$to \
+  | sed -E 's@([^\t]+)\t(.*[(]#([^)]+)[)].*)@\1\thttps://github.com/servo/servo/pull/\3\t\2@'
+done
