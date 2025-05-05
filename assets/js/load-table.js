48d0455e6e965e34963ebd8d3f4f326d5785422f
Oriol Brufau <obrufau@igalia.com>
2025-05-05 22:09:51 +0200

    Stop using `area_keys`
    See https://github.com/servo/internal-wpt-dashboard/issues/62
    
    Signed-off-by: Oriol Brufau <obrufau@igalia.com>

--- assets/js/load-table.js
+++ assets/js/load-table.js
@@ -13,14 +13,14 @@ function update_table (scores) {
     const score_table = document.getElementById('score-table-body')
     removeChildren(score_table)
 
-    for (const [idx, area] of scores.area_keys.entries()) {
+    for (const [idx, area] of scores.focus_areas.entries()) {
         const area_score = scores.scores_last_run[idx + AREA_SCORE_OFFSET]
         const score = Math.floor(1000 * area_score.total_score / area_score.total_tests) / 10
         const subtests = Math.floor(1000 * area_score.total_subtests_passed / area_score.total_subtests) / 10
         score_table.insertAdjacentHTML(
             'beforeend',
             `<tr class="${idx % 2 ? 'odd' : 'even'}">
-                <td>${scores.focus_areas[area]}</td>
+                <td>${area}</td>
                 <td class="score">${String(score).padEnd(4, '.0')}%</td>
                 <td class="score">(${new Intl.NumberFormat().format(area_score.total_subtests_passed)}/${new Intl.NumberFormat().format(area_score.total_subtests)}) ${String(subtests).padEnd(4, '.0')}%</td>
             </tr>`
